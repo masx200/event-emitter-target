28abac1ee8f52f5b45109891115af9ea24f3c629
masx200 <34191203+masx200@users.noreply.github.com>
2020-02-07 00:23:48 +0800

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,91 +1,38 @@
-// import typescriptlib from "typescript";
-//import babel from "rollup-plugin-babel";
-import sourcemaps from "rollup-plugin-sourcemaps";
-import resolve from "rollup-plugin-node-resolve";
-import commonjs from "rollup-plugin-commonjs";
-import { terser } from "rollup-plugin-terser";
-import json from "rollup-plugin-json";
-import typescript from "rollup-plugin-ts";
-/*const beautifyterserplugin = terser({
-  compress: false,
-  mangle: false,
-  output: {
-    comments: !1,
-    beautify: true
-  }
-});*/
-/*const mybabelplugin = babel({
-  plugins: ["@babel/plugin-proposal-optional-catch-binding"],
-  presets: [
-    [
-      "@babel/preset-env",
-      {
-        targets: [
-          "last 1 edge version",
-          "last 1 safari version",
-          "last 1 chrome version",
-          "last 1 firefox version"
-        ]
-      }
-    ]
-  ]
-});*/
+ import sourcemaps from 'rollup-plugin-sourcemaps';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
+import json from '@rollup/plugin-json';
+import typescript from 'rollup-plugin-ts';
 const myterserplugin = terser({
-  sourcemap: true,
-  toplevel: true,
-  output: {
-    comments: !1,
-    ascii_only: !0
-  },
-  compress: {
+    sourcemap: true,
     toplevel: true,
-    unused: true,
-    drop_console: true,
-    drop_debugger: true,
-    pure_funcs: ["console.log"]
-  },
-  mangle: { properties: false }
+    output: {
+        comments: !1,
+        ascii_only: !0
+    },
+    compress: {
+        toplevel: true,
+        unused: true,
+        drop_console: true,
+        drop_debugger: true,
+        pure_funcs: ['console.log']
+    },
+    mangle: { properties: false }
 });
-export default [
-  {
-    input: "./src/index.ts",
-    output: [
-      {
-        file: "./dist/index.js",
-        format: "esm",
-        sourcemap: true
-      }
-    ],
-    plugins: [
-   //   mybabelplugin,
-      sourcemaps(),
-      json(),
-      resolve(),
-      commonjs(),
-      typescript({
-        // tsconfig: "./tsconfig.json",
-        // typescript: typescriptlib
-      }),
-    //  beautifyterserplugin
-    myterserplugin,
-    ]
-  }
-/*,
-  {
-    input: "./dist/index.js",
-    output: [
-      {
-        file: "./dist/index.min.js",
-        format: "esm",
-        sourcemap: true
-      }
-    ],
-    plugins: [
-      mybabelplugin,
-      resolve(),
-      commonjs(),
-      myterserplugin,
-      sourcemaps()
-    ]
-  }*/
-];
+export default [{
+        input: './src/index.ts',
+        output: [{
+                file: './dist/index.js',
+                format: 'esm',
+                sourcemap: true
+            }],
+        plugins: [
+            sourcemaps(),
+            json(),
+            resolve(),
+            commonjs(),
+            typescript({}),
+            myterserplugin
+        ]
+    }];
